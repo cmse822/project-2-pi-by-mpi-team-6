eb797a2f187d20ac0f4c6ec2af3568894eee7cc1
Chris Nosowsky <chrisnosowsky@gmail.com>
2024-02-06 13:29:40 -0500

    add initial implementation of  MPI PI

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,44 @@
+#include <mpi.h>
 #include <iostream>
 using namespace std;
 
-int main() 
+
+
+int main(int argc, char **argv) 
 {
-    cout << "Hello, World!" << endl;
+    MPI_Status Stat;
+    int dest;
+    int source;
+    const char* imsg = "Hello dude";
+    const char* outmsg = "Hello world";
+    int tag = 1;
+    int rc;
+    int count;
+    int numtasks;
+    int rank;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
+    if(rank == 0) {
+        dest = 1;
+        source = 1;
+        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        cout << "I send out msg to 1 rank: " << outmsg << endl;
+	rc = MPI_Recv(&imsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+    }
+    else if (rank == 1) {
+        dest = 0;
+        source = 0;
+        rc = MPI_Recv(&imsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+        cout << "I receive msg from 0 rank guy: " << imsg << endl;
+	rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    }
+    //cout << "Hello, World!" << endl;
+
+
+    MPI_Finalize();
     return 0;
 }
--- ser_pi_calc.cpp
+++ ser_pi_calc.cpp
@@ -10,26 +10,50 @@
 ***************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <mpi.h>
 
+// SIde note: Reduce VS code version to 1.85 if having trouble using ssh tunnel
 void srandom (unsigned seed);  
 double dboard (int darts);
 
 #define DARTS 10000   	/* number of throws at dartboard */
 #define ROUNDS 100    	/* number of times "darts" is iterated */
+#define THRESHOLD 1
 
 int main(int argc, char *argv[])
 {
 double pi;          	/* average of pi after "darts" is thrown */
 double avepi;       	/* average pi value for all iterations */
 int i, n;
+MPI_Status Stat;
+int dest;
+int source;
+int rc;
+int count;
+int numtasks;
+int rank;
+
+MPI_Init(&argc,&argv);
+MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 printf("Starting serial version of pi calculation using dartboard algorithm...\n");
 srandom (5);            /* seed the random number generator */
+
+
 avepi = 0;
+double overall_avepi = 0;
+// darts / num_tasks = number for each thread = 10000/4 = 2500
+// each process wil do 2500 dart throws at board and get average pi
+// MPI_SUM average pi's from each process
 for (i = 0; i < ROUNDS; i++) {
    /* Perform pi calculation on serial processor */
-   pi = dboard(DARTS);
+   pi = dboard(DARTS / numtasks);
    avepi = ((avepi * i) + pi)/(i + 1); 
+   // MPI_REDUCE HERE WITH MPI_SUM as ARGUMENT
+   MPI_Reduce(&avepi, &overall_avepi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
    printf("   After %3d throws, average value of pi = %10.8f\n",
          (DARTS * (i + 1)),avepi);
    }    
@@ -37,11 +61,32 @@ printf("\nReal value of PI: 3.1415926535897 \n");
 }
 
 
+
 /*****************************************************************************
  * dboard
  *****************************************************************************/
 #define sqr(x)	((x)*(x))
 long random(void);
+
+// double get_norm(double x1, double y1) {
+//    return sqrt(sqr(x1) + sqr(y1));
+// }
+
+
+// two vectors
+// for loop over one of the vectors, same size
+// call get_norm
+// start with 0
+
+// int get_total_inner_points(std::vector<int> x1, std::vector<int> y1) {
+//    int number_of_points = 0;
+//    for (int i = 0; i < x1.size(); i++) {
+//       if(get_norm(x1[i], y1[i]) < THRESHOLD) {
+//          number_of_points++;
+//       }
+//    }
+//    return number_of_points;
+// }
 
 double dboard(int darts)
 {
