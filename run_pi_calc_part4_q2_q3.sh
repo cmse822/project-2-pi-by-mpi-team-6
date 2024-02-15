350f12a6c60426eeee6fa38dcc4fb9baa5db3c8c
Chris Nosowsky <chrisnosowsky@gmail.com>
2024-02-15 00:31:17 -0500

    Part 4 Q4,Q5,Q7. Add plots. Update SB script. Waiting for 1E9 to Finish.

--- run_pi_calc_part4_q2_q3.sh
+++ run_pi_calc_part4_q2_q3.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+# Original project darts: 10000
+# Original project rounds: 100
+
+# Below for Onur
+# mpic++ -o ser_pi_calc ser_pi_calc.cpp -std=c++17 -O3
+
+# Below for HPCC
 mpicxx -o ser_pi_calc ser_pi_calc.cpp -std=c++17 -O3
 
 # Check if file exists, if exist delete it
 if [ -f "data.csv" ]; then
     rm data.csv
 fi
-# 1E3 1E6 1E9 darts
-for num_darts in 1000 1000000 1000000000
+
+for num_darts in 10000
 do
-    # 1 2 4 8 16 32 64 ranks
-    for rank_count in 1 2 4 8 16 32 64
+    for rank_count in 1 2 3 4
     do
         for num_rounds in 100
         do
             echo "Running with $rank_count ranks, $num_darts darts, and $num_rounds rounds"
+            # Below for Onur
+            # mpirun -np $rank_count ./ser_pi_calc $num_rounds $num_darts 
             mpiexec -n $rank_count ./ser_pi_calc $num_rounds $num_darts
             echo "Finished "
         done
